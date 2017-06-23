8cf9e9f4e25542d942fcf7964059a572ac81f07f
Leonard <kostaleonard@gmail.com>
2017-06-22 20:17:05 -0400

    Minor changes to effect.
    Signed-off-by: Leonard <kostaleonard@gmail.com>

--- src/main/scala/effects/Effect.scala
+++ src/main/scala/effects/Effect.scala
@@ -19,8 +19,10 @@ case class Berserked(turnsRemaining: Int) extends Effect
 
 case class Burned(turnsRemaining: Int, armorRatingPenalty: Int, damagePerTurn: Int) extends Effect
 
-case class Chilled(turnsRemaining: Int, armorRatingPenalty: Int, movementPenalty: Int) extends Effect
+case class Chilled(turnsRemaining: Int, armorRatingPenalty: Int, movementPenalty: Int, eezoRecoveryPenalty: Int) extends Effect
 
 case class Bleeding(turnsRemaining: Int, damagePerTurn: Int) extends Effect
+
+case class Electrocuted(turnsRemaining: Int, shieldRecoveryPenalty: Int) extends Effect
 
 case class BioticInitiator(turnsRemaining: Int, initiatorPower: Power) extends Effect
